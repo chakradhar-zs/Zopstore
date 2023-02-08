f3ddd6360ecf25e97aa0e67f880a35e66bc552c6
vaishnav-zs <vaishnav.v@zopsmart.com>
2023-02-08 19:15:26 +0530

    rename cases to tests

--- 05-Calculator/calculator_test.go
+++ 05-Calculator/calculator_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func TestCalculator(t *testing.T) {
-	cases := []struct {
+	tests := []struct {
 		op   string
 		a    float64
 		b    float64
@@ -16,7 +16,7 @@ func TestCalculator(t *testing.T) {
 		{"*", 2, 3, 6},
 		{"/", 6, 3, 2},
 	}
-	for _, c := range cases {
+	for _, c := range tests {
 		got := calculator(c.op, c.a, c.b)
 		if got != c.want {
 			t.Errorf("calculator(%s, %f, %f) == %f, want %f", c.op, c.a, c.b, got, c.want)
