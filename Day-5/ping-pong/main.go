6424b691cc9efe9170784fc8f15ecf44f43e97be
vaishnav-zopsmart <77275291+vaishnav-zopsmart@users.noreply.github.com>
2023-02-22 22:47:49 +0530

    Day 5/questions (#7)
    * add ping-pong server
    
    * Day-5| Changing the problem statement of products to GET instead of CREATE
    
    ---------
    
    Co-authored-by: Mithali <mithali.rs@zopsmart.com>

---
+++ Day-5/ping-pong/main.go
@@ -0,0 +1,5 @@
+package main
+
+// Write a Go program that listens for HTTP requests on port 8080.
+// The program should handle requests to the /ping endpoint by responding with the string "pong"
+// Call the handler as 'pingHandler'
